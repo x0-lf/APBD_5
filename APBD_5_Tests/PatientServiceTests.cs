e6a469460b6e7ae8f70f0331de35ce3de2c40378
x0-lf <s19568@pjwstk.edu.pl>
2025-05-24 02:08:45 +0200

    feat: Simple Unit Tests -> Moving APBD_5 to seperate folder because created the Unit Tests

---
+++ APBD_5_Tests/PatientServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using APBD_5.DTOs;
+using APBD_5.Repositories;
+using APBD_5.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace APBD_5;
+
+public class PatientServiceTests
+{
+    private readonly Mock<IPatientRepository> _patientRepoMock = new();
+    private readonly PatientService _patientService;
+
+    public PatientServiceTests()
+    {
+        var logger = new LoggerFactory().CreateLogger<PatientService>();
+        _patientService = new PatientService(_patientRepoMock.Object, logger);
+    }
+
+    [Fact]
+    public async Task ShouldReturnNullWhenPatientNotFound()
+    {
+        _patientRepoMock.Setup(r => r.GetPatientWithPrescriptionsAsync(It.IsAny<int>()))
+            .ReturnsAsync((PatientResponseDto?)null);
+
+        var result = await _patientService.GetPatientAsync(1);
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task ShouldReturnPatientWhenExists()
+    {
+        var dto = new PatientResponseDto
+        {
+            IdPatient = 1,
+            FirstName = "Test"
+        };
+
+        _patientRepoMock.Setup(r => r.GetPatientWithPrescriptionsAsync(1))
+            .ReturnsAsync(dto);
+
+        var result = await _patientService.GetPatientAsync(1);
+        Assert.Equal(1, result?.IdPatient);
+    }
+}
---
+++ APBD_5_Tests/PrescriptionServiceTests.cs
@@ -0,0 +1,93 @@
+﻿using APBD_5.DTOs;
+using APBD_5.Repositories;
+using APBD_5.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace APBD_5;
+
+public class PrescriptionServiceTests
+{
+    private readonly Mock<IPrescriptionRepository> _prescriptionRepoMock = new();
+    private readonly PrescriptionService _prescriptionService;
+
+    public PrescriptionServiceTests()
+    {
+        var logger = new LoggerFactory().CreateLogger<PrescriptionService>();
+        _prescriptionService = new PrescriptionService(_prescriptionRepoMock.Object, logger);
+    }
+
+    [Fact]
+    public async Task ShouldThrowWhenMoreThan10Medicaments()
+    {
+        var dto = new PrescriptionCreateRequestDto
+        {
+            Patient = new PatientDto
+            {
+                FirstName = "Test",
+                LastName = "Test",
+                BirthDate = new DateOnly(1999, 11, 1)
+            },
+            IdDoctor = 1,
+            Date = new DateOnly(2025, 1, 1),
+            DueDate = new DateOnly(2025, 5, 5),
+            Medicaments = Enumerable.Range(1, 11).Select(i => new PrescriptionMedicamentDto
+            {
+                IdMedicament = i,
+                Dose = 1,
+                Details = "Test"
+            }).ToList()
+        };
+
+        await Assert.ThrowsAsync<ArgumentException>(() => _prescriptionService.CreatePrescriptionAsync(dto));
+    }
+
+    [Fact]
+    public async Task ShouldThrowWhenDueDateBeforeDate()
+    {
+        var dto = new PrescriptionCreateRequestDto
+        {
+            Patient = new PatientDto
+            {
+                FirstName = "Test",
+                LastName = "Test",
+                BirthDate = new DateOnly(1999, 11, 8)
+            },
+            IdDoctor = 1,
+            Date = new DateOnly(2025, 9, 10),
+            DueDate = new DateOnly(2025, 8, 1),
+            Medicaments = new List<PrescriptionMedicamentDto>()
+        };
+
+        await Assert.ThrowsAsync<ArgumentException>(() => _prescriptionService.CreatePrescriptionAsync(dto));
+    }
+
+    [Fact]
+    public async Task ShouldCallRepositoryOnValidRequest()
+    {
+        var dto = new PrescriptionCreateRequestDto
+        {
+            Patient = new PatientDto
+            {
+                FirstName = "Valid",
+                LastName = "Patient",
+                BirthDate = new DateOnly(2000, 6, 1)
+            },
+            IdDoctor = 1,
+            Date = new DateOnly(2025, 1, 1),
+            DueDate = new DateOnly(2025, 1, 11),
+            Medicaments = Enumerable.Range(1, 2).Select(i => new PrescriptionMedicamentDto
+            {
+                IdMedicament = i,
+                Dose = 1,
+                Details = "Test"
+            }).ToList()
+        };
+
+        _prescriptionRepoMock.Setup(r => r.CreatePrescriptionAsync(It.IsAny<PrescriptionCreateRequestDto>()))
+            .ReturnsAsync(123);
+
+        var id = await _prescriptionService.CreatePrescriptionAsync(dto);
+        Assert.Equal(123, id);
+    }
+}
